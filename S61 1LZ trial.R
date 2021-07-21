ac7e2c6705fd844b67a81cd644bcd763387c6ab9
meng le <mzhang@hotmail.co.uk>
2021-07-21 10:07:43 +0100

    randomisation within area
    randomising/ altering frontiers based on whole of sheffield and rotherham leads to unsatisfying alternative.
    
    goal: try to generate alternatives based on border stats within Masborough
    
    -[ ] 1. Randomise based on this area
    - [ ]  2. alternative based on frontierness in this area
    - [ ] 3. get stats on alternatives in terms of n. borders, total border length., diff_phi etc

--- S61 1LZ trial.R
+++ S61 1LZ trial.R
@@ -33,6 +33,57 @@ shef_borders <-
   )
 
 
+###Centre of Masborough, Henley and Ferham = S61 1LZ
+
+ukgrid = "+init=epsg:27700"
+
+#Find coord for S61 1LZ
+# pcd_df <- read.csv('temp data/NSPL_FEB_2020_UK/Data/NSPL_FEB_2020_UK.csv')
+# pcd_df %>% head
+# pcd_df$pcd %>% grep('S61 1LZ', x = .) #1981275
+# pcd_df$oseast1m[1981275] #441515
+# pcd_df$osnrth1m[1981275] #393038
+
+#Make S61_1LZ a point
+S61_1LZ <-
+  st_point(
+    c(441515, 393038)
+  )
+
+S61_1LZ <-
+  S61_1LZ %>%
+  st_sfc(
+    crs = st_crs(ukgrid)
+  )
+
+S61_1LZ %>% qtm
+
+S61_1LZ <-
+  S61_1LZ %>% 
+  st_buffer(2000)
+
+S61_1LZ %>% qtm
+
+#S61_1LZ %>% saveRDS('cleaned data/S61 1LZ borders.rds')
+
+
+# subset ------------------------------------------------------------------
+
+shef_borders <-
+  shef_borders[S61_1LZ, ]
+
+shef_borders %>% qtm
+
+## 1. Randomise based on this area
+##  2. alternative based on frontierness in this area 
+##  3. get stats on alternatives in terms of n. borders, total border length., diff_phi etc
+
+shef_borders %>% st_length()
+shef_borders %>% nrow
+# -------------------------------------------------------------------------
+
+
+
 ###
 mapA <- 
   shef_borders %>%
@@ -57,15 +108,12 @@ set.seed(123) # sets random number gen
 rand.index <- 
   sample.int(nrow(shef_borders), 242)
 
-rand.index
 
 mapD <-
   shef_borders[rand.index, ]
-mapD
 
-mapD$rank %>% summary
-
-
+mapE <- 
+  shef_borders  ##all borders 
 
 
 ### Collate
@@ -73,7 +121,7 @@ mapD$rank %>% summary
 set.seed(444)
 order_id <- 
 #  sample.int(4, 4)
-  1:4
+  1:5
 typeName <- 
   letters[order_id]
 
@@ -83,42 +131,11 @@ combined_maps <-
     mapA %>% mutate(type = typeName[1]),
     mapB %>% mutate(type = typeName[2]),
     mapC %>% mutate(type = typeName[3]),
-    mapD %>% mutate(type = typeName[4])
+    mapD %>% mutate(type = typeName[4]),
+    mapE %>% mutate(type = typeName[5])
   )
 
 
-###Centre of Masborough, Henley and Ferham = S61 1LZ
-
-ukgrid = "+init=epsg:27700"
-
-#Find coord for S61 1LZ
-pcd_df <- read.csv('temp data/NSPL_FEB_2020_UK/Data/NSPL_FEB_2020_UK.csv')
-pcd_df %>% head
-pcd_df$pcd %>% grep('S61 1LZ', x = .) #1981275
-pcd_df$oseast1m[1981275] #441515
-pcd_df$osnrth1m[1981275] #393038
-
-#Make S61_1LZ a point
-S61_1LZ <-
-  st_point(
-    c(441515, 393038)
-  )
-
-S61_1LZ <-
-  S61_1LZ %>%
-  st_sfc(
-    crs = st_crs(ukgrid)
-  )
-
-S61_1LZ %>% qtm
-
-S61_1LZ <-
-  S61_1LZ %>% 
-  st_buffer(2000)
-
-S61_1LZ %>% qtm
-
-#S61_1LZ %>% saveRDS('cleaned data/S61 1LZ borders.rds')
 ## subset
 these_borders <- 
    combined_maps[S61_1LZ, ]
@@ -135,17 +152,11 @@ tm_shape(S61_1LZ) +
   tm_borders(alpha = 0.5) +
   tm_shape(
     these_borders %>% 
-      filter(type %in% c('a', 'b'))
+      filter(type %in% 
+               c('a', 'b', 'c', 'd', 'e')
+             )
   ) +
   tm_lines(lwd = 3) +
   tm_facets('type', sync = T)
 
 
-tm_shape(S61_1LZ) + 
-  tm_borders(alpha = 0.5) +
-  tm_shape(
-    these_borders %>% 
-      filter(type %in% c('c', 'd'))
-  ) +
-  tm_lines(lwd = 3) +
-  tm_facets('type', sync = T)
