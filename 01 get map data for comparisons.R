e951a1ac9a1b712e996fbba00fe9c6881f80db6c
meng le <mzhang@hotmail.co.uk>
2021-07-23 15:52:02 +0100

    cleanup of unused files/ tweaked code for neatness/ WIP commited for merge
    1. Scripts to make maps now 01->02
    2. moved kimberworth, crookes, Ruct, sheff uni coords in 01
    3. deleted trial maps and old viz scripts -- outdated
    
    still wip as to how we cut the map (thirds or quartile or something else)

--- 01 get map data for comparisons.R
+++ 01 get map data for comparisons.R
@@ -4,16 +4,14 @@
 # mapA <- #Qs 1
 # mapB <- #Qs 2
 # mapC <- #Qs 3
-# mapD <- # random map
-# mapE <- #NA
-# 
 
 
 # input/ output -----------------------------------------------------------
 
-## input
-shefBase_Here <- 
-  'cleaned data/makeFile01 sheffield ttwa lsoa.rds'
+## input: 
+## Base only if we can to map prop Foreigners
+# shefBase_Here <- 
+#   'cleaned data/makeFile01 sheffield ttwa lsoa.rds'
 
 shefBorders_Here <-
   'cleaned data/makeFile01 sheffield borders+frontiers.rds'
@@ -21,17 +19,12 @@ shefBorders_Here <-
 
 
 # 1. read in the data -----------------------------------------------------
-
-shef_sf <-
-  shefBase_Here %>% readRDS
+# 
+# shef_sf <-
+#   shefBase_Here %>% readRDS
 
 shef_borders <-
   shefBorders_Here %>% readRDS
-
-
-##  Example maybe using 85% - 100% and 75% - 90% -- throws people off?
-##  or 3 level add in 65% - 80%
-##  We have 3 choices then so we sets of comparisons 3^1 choiceset
 
 ## Add percentile ranks
 
@@ -39,115 +32,132 @@ shef_borders <-
   shef_borders %>%
   arrange(std_diff_phi) %>%
   mutate(
-    order = 1:nrow(shef_borders),
-    rank = order / (nrow(shef_borders)) * 100
+    rank = ntile(std_diff_phi, 100)
   )
 
+# 2. Pick a centrePoint ---------------------------------------------------
 
-###Centre of Masborough, Henley and Ferham = S61 1LZ
 
 ukgrid = "+init=epsg:27700"
 
 #Find coord for S61 1LZ
+##  example code for check postcode
 # pcd_df <- read.csv('temp data/NSPL_FEB_2020_UK/Data/NSPL_FEB_2020_UK.csv')
 # pcd_df %>% head
 # pcd_df$pcd %>% grep('S61 1LZ', x = .) #1981275
 # pcd_df$oseast1m[1981275] #441515
 # pcd_df$osnrth1m[1981275] #393038
 
-#Make S61_1LZ a point
-S61_1LZ <-
+
+##  Find coordinates of postcode point to centre
+# Centre of Masborough, Henley and Ferham = S61 1LZ
+S61_1LZ <- 
+  c(441515, 393038)
+###Centre of Kimberworth = S61 1PB
+S61_1PB <-
+  c(440243, 393689)
+###Centre of Crookes = S10 1TE
+S10_1TE <-
+    c(432798, 387752)
+##  Sheff uni = S10 2TN
+shef_uni <-
+    c(434120, 387290)
+# RUCT =  S60 1AH
+RUCST <-
+    c(442732, 392677)
+
+
+##  Create gis obj for centre
+centrePoint <-
   st_point(
-    c(441515, 393038)
+    S61_1LZ
   )
 
-S61_1LZ <-
-  S61_1LZ %>%
+centrePoint <-
+  centrePoint %>%
   st_sfc(
     crs = st_crs(ukgrid)
   )
 
 # buffer point ------------------------------------------------------------
 
-
-tmap_mode('view')
-
-S61_1LZ <-
-  S61_1LZ %>% 
+centrePoint <-
+  centrePoint %>% 
   st_buffer(1500)
 
+centrePoint %>% qtm
 
-
-# subset and make quartiles -------------------------------------------------
+# 3. subset borders and make xtiles ---------------------------------------
 
 rel_borders <-
   shef_borders %>%
-  st_intersection(S61_1LZ)
+  st_intersection(centrePoint)
 
-#create relative rank just for Masborough area
+rel_borders 
+# create xtile group based on std_diff_phi 
+##  See ntile for making quartiles, thirds etc
 rel_borders <-
   rel_borders %>%
   arrange(std_diff_phi) %>%
   mutate(
-    order_rel = 1:nrow(rel_borders),
-    rank_rel = order_rel / (nrow(rel_borders)) * 100,
-    quartile_rel = ntile(-std_diff_phi, 3)
+    xtile_rel = ntile(-std_diff_phi, 3) 
   )
 
+## Check statistics
 rel_borders$length <-
   rel_borders %>% st_length()
 
 ##  Summaries; Q1 = highest percentile group
 rel_borders %>% 
-  group_by(quartile_rel) %>%
+  group_by(xtile_rel) %>%
   select(std_diff_phi:length) %>%
   summarise_all( mean ) 
 
 rel_borders %>% summary ## check for the stat vs the random map
 
 
-# create maps  ------------------------------------------------------------
-
+# 4. create map data  -------------------------------------------------------
 
 mapA <- #Qs 1
   rel_borders %>%
   filter(
-    quartile_rel %in% c(1)
+    xtile_rel %in% c(1)
   ) 
 
 mapB <- #Qs 2
   rel_borders %>%
   filter(
-    quartile_rel %in% c(2)
+    xtile_rel %in% c(2)
   ) 
 
 mapC <- #Qs 3
   rel_borders %>%
   filter(
-    quartile_rel %in% c(3)
+    xtile_rel %in% c(3)
   ) 
 
 # mapD <- #Qs 4
 #   rel_borders %>%
 #   filter(
-#     quartile_rel %in% c(4)
+#     xtile_rel %in% c(4)
 #   ) 
 # 
 # mapE <- #Qs 1&3
 #   rel_borders %>%
 #   filter(
-#     quartile_rel %in% c(1, 3)
+#     xtile_rel %in% c(1, 3)
 #   ) 
 
 
-# save maps ---------------------------------------------------------------
+# End. save maps ---------------------------------------------------------------
 mapdataList <-
   list(
     a = mapA,
     b = mapB,
-    c = mapC#,
+    c = mapC,
 #    d = mapD#,
 #    e = mapE
+    centrePoint = centrePoint
   )
 
 
--- 02 randomise pairs & call rmd to make maps.R
+++ 02 randomise pairs & call rmd to make maps.R
@@ -1,15 +1,16 @@
 
-# Randomise pairs and save maps -------------------------------------------
+# 02: Randomise pairs and save maps -------------------------------------------
 
 
 # inputs ------------------------------------------------------------------
 
 mapdataList <- readRDS('cleaned data/01 data for trial maps.rds')
-
+centrePoint <- mapdataList$centrePoint
 
 
 # 1. Create map pairs and randomise which map is on lhs ----------------------
 
+## Current version = thirds 
 # refresher on map order
 # mapA <- #Qs 1
 # mapB <- #Qs 2
@@ -32,20 +33,20 @@ map_b_position <-
 
 pair_1 <-
   bind_rows(
-    mapdataList$a %>% mutate(type = map_a_position[1], pair = 1),
-    mapdataList$b %>% mutate(type = map_b_position[1], pair = 1),
+    mapdataList$a %>% mutate(type = map_a_position[1]),
+    mapdataList$b %>% mutate(type = map_b_position[1]),
   )
 
 pair_2 <-
   bind_rows(
-    mapdataList$a %>% mutate(type = map_a_position[2], pair = 2),
-    mapdataList$c %>% mutate(type = map_b_position[2], pair = 2),
+    mapdataList$a %>% mutate(type = map_a_position[2]),
+    mapdataList$c %>% mutate(type = map_b_position[2]),
   )
 
 pair_3 <-
   bind_rows(
-    mapdataList$b %>% mutate(type = map_a_position[3], pair = 3),
-    mapdataList$c %>% mutate(type = map_b_position[3], pair = 3),
+    mapdataList$b %>% mutate(type = map_a_position[3]),
+    mapdataList$c %>% mutate(type = map_b_position[3]),
   )
 
 # pair_4 <-
@@ -60,16 +61,14 @@ all_pairs <-
        )
 
 
-# create the tmap objs  ---------------------------------------------------
+# 2. create the tmap objs  ---------------------------------------------------
 
 #tmap_mode('view')
 
 
 pairMap1 <- 
-  tm_shape(S61_1LZ) + 
+  tm_shape(centrePoint) + 
   tm_borders(alpha = 0.5) +
-  # tm_shape(base_layer) +
-  # tm_fill('propFor', alpha = 0.2) +
   tm_shape(
     pair_1
   ) +
@@ -77,10 +76,8 @@ pairMap1 <-
   tm_facets('type', sync = T)
 
 pairMap2 <- 
-  tm_shape(S61_1LZ) + 
+  tm_shape(centrePoint) + 
   tm_borders(alpha = 0.5) +
-  # tm_shape(base_layer) +
-  # tm_fill('propFor', alpha = 0.2) +
   tm_shape(
     pair_2
   ) +
@@ -88,27 +85,25 @@ pairMap2 <-
   tm_facets('type', sync = T)
 
 pairMap3 <- 
-  tm_shape(S61_1LZ) + 
+  tm_shape(centrePoint) + 
   tm_borders(alpha = 0.5) +
-  # tm_shape(base_layer) +
-  # tm_fill('propFor', alpha = 0.2) +
   tm_shape(
     pair_3
   ) +
   tm_lines(lwd = 3) +
   tm_facets('type', sync = T)
 
-pairMap4 <- 
-  tm_shape(S61_1LZ) + 
-  tm_borders(alpha = 0.5) +
-  # tm_shape(base_layer) +
-  # tm_fill('propFor', alpha = 0.2) +
-  tm_shape(
-    pair_4
-  ) +
-  tm_lines(lwd = 3) +
-  tm_facets('type', sync = T)
-
+# pairMap4 <- 
+#   tm_shape(centrePoint) + 
+#   tm_borders(alpha = 0.5) +
+#   # tm_shape(base_layer) +
+#   # tm_fill('propFor', alpha = 0.2) +
+#   tm_shape(
+#     pair_4
+#   ) +
+#   tm_lines(lwd = 3) +
+#   tm_facets('type', sync = T)
+# 
 
 pairMapList <-
   list(
@@ -119,7 +114,7 @@ pairMapList <-
   )
 
 
-# Randomise map pairs and save the order ----------------------------------
+# 3. Randomise map pairs and save the order ----------------------------------
 
 ## Create directory
 map.dir <- 
@@ -181,4 +176,4 @@ rmarkdown::render(
 #   params = list())
 
 
-#
+## End
