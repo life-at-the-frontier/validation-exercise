209033a1ca51ea0e6f00a7530b5462f9979c717c
Meng Le Zhang <mzhang@hotmail.co.uk>
2021-07-23 16:46:41 +0100

    created random maps + saved (#3)
    Created cleanish maps and randomising routine 
    
    issue: basically masborough was picked beforehand to be frontier dense. so only the 'real' frontier map A is populated -- other maps are less populated if we picked based on percentile ranks based on all sheffield.
    
    So therefore we tried various map sets to see how to fix this problem:
    - randomise/ chose within the area instead whole TTWA (e.g. masborough only)
    - use output area frontiers
    - smaller changes in frontierness between alternatives???
    
    See tags archive/[branch] to see trials.
    
    Eventually settled on 1) choose frontiers in an area and 2) comparing border groups ranked by their diff_phi. WIP still on whether to use thirds etc. We generated working code to create map pairs (randomised within pairs and between pair sets) and to output them for the interview team. 
    
    Major changes
    1. draft write up for dissemination sent 22/7/2021 to team
    2. Tried many alternatives (see archive/ tags)
    2.1. One idea was to compare maps with 2 quartiles per map at a time but trial with crookes took a while to complete 4 pairs (~10 min). Hence we now compare only 1 quartile (or third) -- see diary notes.
    
    3. 01 extracts the necessary map data used in 02. This is where we divide borders into quartiles (or other ranking groups)
    
    4. Renders map by called 02 to render rmd file.
    4.1 Cannot use tmap_save to save  facetted tmaps (currently an issue w/ tmap)
    
    5. cleanup of unused files/ tweaked code for neatness/ WIP commited for merge
    
    1. Scripts to make maps now 01->02, also rmd files to rend them
    2. moved kimberworth, crookes, Ruct, sheff uni coords in 01
    3. deleted trial maps and old viz scripts -- outdated
    
    6. updated gitignore + readme
    
    
    todo: WIP as to how we cut the map (thirds or quartile or something else)
    
    Co-authored-by: Lydia Warden <ljwarden1@sheffield.ac.uk>

---
+++ 01 get map data for comparisons.R
@@ -0,0 +1,164 @@
+# 01. Make maps for each pair -------------------------------------------------
+
+##  Note: maps are now thirds Q = top third
+# mapA <- #Qs 1
+# mapB <- #Qs 2
+# mapC <- #Qs 3
+
+
+# input/ output -----------------------------------------------------------
+
+## input: 
+## Base only if we can to map prop Foreigners
+# shefBase_Here <- 
+#   'cleaned data/makeFile01 sheffield ttwa lsoa.rds'
+
+shefBorders_Here <-
+  'cleaned data/makeFile01 sheffield borders+frontiers.rds'
+
+
+
+# 1. read in the data -----------------------------------------------------
+# 
+# shef_sf <-
+#   shefBase_Here %>% readRDS
+
+shef_borders <-
+  shefBorders_Here %>% readRDS
+
+## Add percentile ranks
+
+shef_borders <-
+  shef_borders %>%
+  arrange(std_diff_phi) %>%
+  mutate(
+    rank = ntile(std_diff_phi, 100)
+  )
+
+# 2. Pick a centrePoint ---------------------------------------------------
+
+
+ukgrid = "+init=epsg:27700"
+
+#Find coord for S61 1LZ
+##  example code for check postcode
+# pcd_df <- read.csv('temp data/NSPL_FEB_2020_UK/Data/NSPL_FEB_2020_UK.csv')
+# pcd_df %>% head
+# pcd_df$pcd %>% grep('S61 1LZ', x = .) #1981275
+# pcd_df$oseast1m[1981275] #441515
+# pcd_df$osnrth1m[1981275] #393038
+
+
+##  Find coordinates of postcode point to centre
+# Centre of Masborough, Henley and Ferham = S61 1LZ
+S61_1LZ <- 
+  c(441515, 393038)
+###Centre of Kimberworth = S61 1PB
+S61_1PB <-
+  c(440243, 393689)
+###Centre of Crookes = S10 1TE
+S10_1TE <-
+    c(432798, 387752)
+##  Sheff uni = S10 2TN
+shef_uni <-
+    c(434120, 387290)
+# RUCT =  S60 1AH
+RUCST <-
+    c(442732, 392677)
+
+
+##  Create gis obj for centre
+centrePoint <-
+  st_point(
+    S61_1LZ
+  )
+
+centrePoint <-
+  centrePoint %>%
+  st_sfc(
+    crs = st_crs(ukgrid)
+  )
+
+# buffer point ------------------------------------------------------------
+
+centrePoint <-
+  centrePoint %>% 
+  st_buffer(1500)
+
+centrePoint %>% qtm
+
+# 3. subset borders and make xtiles ---------------------------------------
+
+rel_borders <-
+  shef_borders %>%
+  st_intersection(centrePoint)
+
+rel_borders 
+# create xtile group based on std_diff_phi 
+##  See ntile for making quartiles, thirds etc
+rel_borders <-
+  rel_borders %>%
+  arrange(std_diff_phi) %>%
+  mutate(
+    xtile_rel = ntile(-std_diff_phi, 3) 
+  )
+
+## Check statistics
+rel_borders$length <-
+  rel_borders %>% st_length()
+
+##  Summaries; Q1 = highest percentile group
+rel_borders %>% 
+  group_by(xtile_rel) %>%
+  select(std_diff_phi:length) %>%
+  summarise_all( mean ) 
+
+rel_borders %>% summary ## check for the stat vs the random map
+
+
+# 4. create map data  -------------------------------------------------------
+
+mapA <- #Qs 1
+  rel_borders %>%
+  filter(
+    xtile_rel %in% c(1)
+  ) 
+
+mapB <- #Qs 2
+  rel_borders %>%
+  filter(
+    xtile_rel %in% c(2)
+  ) 
+
+mapC <- #Qs 3
+  rel_borders %>%
+  filter(
+    xtile_rel %in% c(3)
+  ) 
+
+# mapD <- #Qs 4
+#   rel_borders %>%
+#   filter(
+#     xtile_rel %in% c(4)
+#   ) 
+# 
+# mapE <- #Qs 1&3
+#   rel_borders %>%
+#   filter(
+#     xtile_rel %in% c(1, 3)
+#   ) 
+
+
+# End. save maps ---------------------------------------------------------------
+mapdataList <-
+  list(
+    a = mapA,
+    b = mapB,
+    c = mapC,
+#    d = mapD#,
+#    e = mapE
+    centrePoint = centrePoint
+  )
+
+
+mapdataList %>% saveRDS('cleaned data/01 data for trial maps.rds')
---
+++ 02 randomise pairs & call rmd to make maps.R
@@ -0,0 +1,179 @@
+
+# 02: Randomise pairs and save maps -------------------------------------------
+
+
+# inputs ------------------------------------------------------------------
+
+mapdataList <- readRDS('cleaned data/01 data for trial maps.rds')
+centrePoint <- mapdataList$centrePoint
+
+
+# 1. Create map pairs and randomise which map is on lhs ----------------------
+
+## Current version = thirds 
+# refresher on map order
+# mapA <- #Qs 1
+# mapB <- #Qs 2
+# mapC <- #Qs 3
+# mapD <- #NA
+
+
+nPairs <- 3 # number of pairs
+
+#Collate in pairs
+#set.seed(123)
+map_a_position <-
+  sample.int(2, nPairs, replace = T)
+
+#map_a_position
+
+map_b_position <-
+  ifelse(map_a_position == 2, 1, 2)
+
+
+pair_1 <-
+  bind_rows(
+    mapdataList$a %>% mutate(type = map_a_position[1]),
+    mapdataList$b %>% mutate(type = map_b_position[1]),
+  )
+
+pair_2 <-
+  bind_rows(
+    mapdataList$a %>% mutate(type = map_a_position[2]),
+    mapdataList$c %>% mutate(type = map_b_position[2]),
+  )
+
+pair_3 <-
+  bind_rows(
+    mapdataList$b %>% mutate(type = map_a_position[3]),
+    mapdataList$c %>% mutate(type = map_b_position[3]),
+  )
+
+# pair_4 <-
+#   bind_rows(
+#     mapE %>% mutate(type = map_a_position[4], pair = 4),
+#     mapC %>% mutate(type = map_b_position[4], pair = 4),
+#   )
+
+all_pairs <- 
+  list(pair_1, pair_2, pair_3#, 
+       #pair_4
+       )
+
+
+# 2. create the tmap objs  ---------------------------------------------------
+
+#tmap_mode('view')
+
+
+pairMap1 <- 
+  tm_shape(centrePoint) + 
+  tm_borders(alpha = 0.5) +
+  tm_shape(
+    pair_1
+  ) +
+  tm_lines(lwd = 3) +
+  tm_facets('type', sync = T)
+
+pairMap2 <- 
+  tm_shape(centrePoint) + 
+  tm_borders(alpha = 0.5) +
+  tm_shape(
+    pair_2
+  ) +
+  tm_lines(lwd = 3) +
+  tm_facets('type', sync = T)
+
+pairMap3 <- 
+  tm_shape(centrePoint) + 
+  tm_borders(alpha = 0.5) +
+  tm_shape(
+    pair_3
+  ) +
+  tm_lines(lwd = 3) +
+  tm_facets('type', sync = T)
+
+# pairMap4 <- 
+#   tm_shape(centrePoint) + 
+#   tm_borders(alpha = 0.5) +
+#   # tm_shape(base_layer) +
+#   # tm_fill('propFor', alpha = 0.2) +
+#   tm_shape(
+#     pair_4
+#   ) +
+#   tm_lines(lwd = 3) +
+#   tm_facets('type', sync = T)
+# 
+
+pairMapList <-
+  list(
+    pairMap1,
+    pairMap2,
+    pairMap3,
+    pairMap4 =NA
+  )
+
+
+# 3. Randomise map pairs and save the order ----------------------------------
+
+## Create directory
+map.dir <- 
+  'output maps'
+
+dir.create(
+  map.dir
+)
+
+##  Then randomise pairs
+
+pairOrder <- 
+  sample.int(nPairs, nPairs)
+
+randPairs <- pairMapList[pairOrder]
+
+## save the randomised order
+randPairs %>% 
+  saveRDS(
+    map.dir 
+    %>% file.path('random map pairs.rds')
+  )
+
+
+##  Save order 
+
+orderFile <-
+  data.frame(
+    shownOrder = 1:nPairs,
+    pairID = pairOrder,
+    map_a_position = map_a_position[pairOrder]
+  )
+
+orderFile %>% 
+  write.csv(
+    map.dir %>% file.path('map order.csv'),
+    row.names = F
+  )
+
+
+
+
+# final step render markdown ----------------------------------------------
+
+rmarkdown::render(
+  map.dir %>% file.path("renderPair1.Rmd"), 
+  params = list())
+
+rmarkdown::render(
+  map.dir %>% file.path("renderPair2.Rmd"), 
+  params = list())
+
+rmarkdown::render(
+  map.dir %>% file.path("renderPair3.Rmd"), 
+  params = list())
+
+# rmarkdown::render(
+#   map.dir %>% file.path("renderPair4.Rmd"), 
+#   params = list())
+
+
+## End
