3424adcf578df961415869876c075def4b122a49
meng le <mzhang@hotmail.co.uk>
2021-07-30 13:10:18 +0100

    created forloop for copying and renaming files

--- 0x copy and rename.R
+++ 0x copy and rename.R
@@ -1,15 +1,88 @@
 ## 0x could we simply just copy and rename the maps we use 
 
 
-## example copy
 
-dir.create('copy')
+# input: specify folders --------------------------------------------------
 
-?file.copy
-file.copy(from = 'output maps/renderPair1.html', to = 'copy')
+origMaps_dir <-
+  'output maps'
 
-## rename
-?file.rename
-file.rename(from = 'copy/renderPair1.html', to = 'copy/dave.html')
+trialMaps_dir <-
+  'maps for experiment'
 
-## rename seems to also move the file ;S -- so we need the full filepath
+##  How many sets of experiments 
+nSets <- 20
+nPairs <- 3
+
+
+# 1. Load set order + initialise folders ----------------------------------
+
+## load in the set order
+setTable <- 'cleaned data/secret map order.rds' %>% readRDS
+
+## create the trial folder
+trialMaps_dir %>% dir.create()
+  
+
+# 2. add the filename for the pair maps to be extracted --------------
+
+setTable <-
+  setTable %>%
+  mutate(
+  choose_files =
+  paste0('pair', realPair, '_', mapA_position, '.html'),
+  rename_to = 
+    paste0('pair', seenOrder, '.html')
+)
+
+# 3. Now to create the routine to create the files in the folders -------
+
+for (set_i in 1:nSets){
+  set_i_dir <-
+    file.path(trialMaps_dir, set_i)
+  
+  set_i_table <-
+    setTable %>%
+    filter(set == set_i)
+  
+  ## create dir
+  set_i_dir %>% dir.create(show)
+
+
+  
+  ## locations  
+  set_i_copyThese <- 
+    file.path(
+      origMaps_dir,
+      set_i_table$choose_files
+    )
+  
+  set_i_saveHere <- 
+    file.path(
+      set_i_dir,
+      set_i_table$choose_files
+    )
+  
+  set_i_renameTo <- 
+    file.path(
+      set_i_dir,
+      set_i_table$rename_to
+    )
+  
+  ## Now to run the copy + rename
+  
+  file.copy(from = set_i_copyThese, 
+            to = set_i_dir,
+            overwrite = T)
+  
+  file.rename(
+    from = set_i_saveHere, 
+    to = set_i_renameTo
+    )
+  
+  ## print to check everything has worked
+  print(set_i)
+  print(set_i_renameTo %>% file.exists)
+}
+
+### End
