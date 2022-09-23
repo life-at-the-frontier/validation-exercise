2df5846ca577d004d3c79151341f5f15f0f57b64
meng <sd>
2022-09-23 10:54:15 +0100

    init date date cleaning and time duration

--- makeFile03 clean experiment.R
+++ makeFile03 clean experiment.R
@@ -4,6 +4,7 @@
 # input -------------------------------------------------------------------
 
 library(tidyverse)
+library(lubridate)
 
 result_df <-
   read_csv('cleaned data/experiment results.csv')
@@ -40,6 +41,51 @@ result_df$interview_id %>% table # check all had 3...
 
 # oh wow most could correct guess! so yes choosing Rho is more or less good
 
+
+# convert durations to seconds --------------------------------------------
+
+names(result_df)
+result_df ## duration1 = total start and end time; duration2 =  time from map 1 to end
+
+convertToSecs <-
+  function(durationStr){
+    (durationStr %>% substr(4,5) %>% as.numeric)*60 + ## minutes
+      (durationStr %>% substr(7,8) %>% as.numeric) #seconds
+    }
+
+
+result_df <-
+  result_df %>%
+  mutate(
+    duration1 = duration1 %>% convertToSecs(),
+    duration2 = duration2 %>% convertToSecs(),
+  ) %>%
+## Set zero duration to NA
+  mutate(
+    duration1 = ifelse(duration1 == 0, NA, duration1),
+    duration2 = ifelse(duration2 == 0, NA, duration2)
+    )
+
+# order by date and add in order index ------------------------------------
+result_df <-
+  result_df %>%
+  mutate(
+    date = date %>% as.Date('%d/%m/%Y')
+    )
+
+date_order <-
+  result_df %>%
+  group_by(interview_id) %>%
+  arrange(date) %>%
+  summarise(
+    chron_id = NA
+  )
+
+date_order$chron_id = 1:nrow(date_order)
+
+result_df <-
+  result_df %>%
+  left_join(date_order)
 
 # save output -------------------------------------------------------------
 
